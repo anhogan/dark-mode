d576fcec1dd8047c377a708283491a0de8214f08
Amanda Hogan <ahogan@teamworks.com>
2020-02-12 10:59:41 -0500

    Adjust so it starts at the right toggle position

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -1,13 +1,13 @@
 import { useEffect } from 'react';
 import useLocalStorage from './useLocalStorage';
 
-const useDarkMode = () => {
-  const [storedValue, setStoredValue] = useLocalStorage('dark-mode', 'toggle');
+const useDarkMode = (key, initialValue) => {
+  const [storedValue, setStoredValue] = useLocalStorage('dark-mode', false);
   
   useEffect(() => {
     const className = 'dark-mode';
     const element = window.document.body;
-    if(!storedValue) {
+    if(storedValue) {
       element.classList.add(className);
     } else {
       element.classList.remove(className);
